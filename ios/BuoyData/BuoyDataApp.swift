5ff9728ed7362d7f8a7fd8d3c98530f49b1d4996
Erik Savage <erikmsavage@gmail.com>
2025-01-06 14:37:09 -0500

    Hello world iOS project + gitignore

---
+++ ios/BuoyData/BuoyDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuoyDataApp.swift
+//  BuoyData
+//
+//  Created by Erik Savage on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BuoyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/BuoyDataTests/BuoyDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuoyDataTests.swift
+//  BuoyDataTests
+//
+//  Created by Erik Savage on 1/6/25.
+//
+
+import Testing
+@testable import BuoyData
+
+struct BuoyDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
